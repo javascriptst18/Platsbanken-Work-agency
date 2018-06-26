b6926d1fa6660c8d7f5ebcc13a94f49aea104a2f
Marcus Hong <Marcus.mh93@gmail.com>
2018-06-26 16:35:57 +0200

    Refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,31 @@
+ // API url to specify county and number of printouts 
+searchByCriteria("platsannonser/matchning?lanid=1&antalrader=10");
+
+ // async for our main call top 10 stockholm + total job application 
 async function searchByCriteria(searchCriteria) {
 	const baseURL = "http://api.arbetsformedlingen.se/af/v0/";
 	const responseObject = await fetch(baseURL + searchCriteria);
 	const matches = await responseObject.json();
 
-	console.log(matches);
+	// console.log(matches);
 
-	match(matches);
+	 // Call to function that creates job application cards 
+	createCards(matches);
+
+	 // Call to function that prints out total job applications 
 	totannonser(matches);
 
 }
 
-function totannonser(matches) {
-	const antal_platsannonser = document.querySelector("#antal_platsannonser");
-	let annonsData = matches.matchningslista;
-	let html = `Totalt antal annonser i Stockholms län: ${annonsData.antal_platsannonser}`;
-	antal_platsannonser.innerHTML = html;
-}
-
-function match(matches) {
+ // int argument is object gained from searchByCritera 
+function createCards(matches) {
+	// Select main with id annons and printout objects from annons
 	const annonsContainer = document.querySelector("#annons");
 	let annonsData = matches.matchningslista.matchningdata;
-	let html = "";
+	let html = ""; // Empty string
 	for (let annons of annonsData) {
 		
-
+		// Append empty string with HTML elements
 		html += `
 			
 			<div id="${annons.annonsid}" class="annons">
@@ -37,10 +39,23 @@ function match(matches) {
 				</a>
 			</div>
 		`;
+
+		// Printout HTML string with HTML elements
 		annonsContainer.innerHTML = html;
 	}
 
 }
 
-searchByCriteria("platsannonser/matchning?lanid=1&antalrader=10");
+// Static function that takes object gained from searchByCriteria as input
+function totannonser(matches) {
+
+	// Select <h3> in header
+	const antal_platsannonser = document.querySelector("#antal_platsannonser");
+	let annonsData = matches.matchningslista;
+	// Printout total number of job applications
+	let html = `Totalt antal annonser i Stockholms län: ${annonsData.antal_platsannonser}`;
+	antal_platsannonser.innerHTML = html;
+}
+
+
 
