515f3d8c557eb6b13324e3e14b7ec02b21320261
Marcus Hong <Marcus.mh93@gmail.com>
2018-06-28 15:32:44 +0200

    completed modal

--- scripts/index.js
+++ scripts/index.js
@@ -25,8 +25,9 @@ async function applicationInfo(searchCriteria) {
 	const baseURL = "http://api.arbetsformedlingen.se/af/v0/";
 	const responseObject = await fetch(baseURL + searchCriteria);
 	const matches = await responseObject.json();
+	console.log(matches);
 
-//	createModal();
+	appendModal(matches);
 	// console.log(matches);
 
 	// createMoreInfo(matches);
@@ -104,6 +105,39 @@ function createModal(matches) {
 	}
 }
 
+function appendModal(matches) {
+	const modalHeader = document.querySelector("#infoHeader");
+	const modalContent = document.querySelector("#modalContent");
+	console.log(matches);
+
+	let headerHTML = `
+	<h2>${matches.platsannons.annons.annonsrubrik}</h2>
+	`;
+
+	modalHeader.innerHTML = headerHTML;
+
+	// modalHeader.insertAdjacentHTML("afterend", `<h2>${matches.platsannons.annons.annonsrubrik}</h2>`)
+
+
+	let html = "";
+
+	html = `
+		<p><b>Arbetsplats: </b>${matches.platsannons.arbetsplats.arbetsplatsnamn}</p>
+		<p><b>Kontaktperson: </b>${matches.platsannons.arbetsplats.kontaktpersonlista.kontaktpersondata[0].namn} </p>
+		<p><b>Tel: </b>${matches.platsannons.arbetsplats.kontaktpersonlista.kontaktpersondata[0].telefonnummer} </p>
+		<p><b>Mail: </b>${matches.platsannons.arbetsplats.kontaktpersonlista.kontaktpersondata[0].epostadress} </p>
+		<p><b>Arbetstid: </b>${matches.platsannons.villkor.arbetstid}</p>
+		<p><b>Varaktighet: </b>${matches.platsannons.villkor.varaktighet}</p>
+		<p>${matches.platsannons.annons.annonstext.substring(0, 500)}...
+			<a href="${matches.platsannons.annons.platsannonsUrl}">gå till annons</a></p>
+
+
+
+	`;
+
+	modalContent.innerHTML = html;
+}
+
 /*function createMoreInfo(matches) {
 
 	const cardContainer = document.getElementById(`${matches.platsannons.annons.annonsid}`);
@@ -132,6 +166,9 @@ formSubmit.addEventListener("submit", function (event) {
 	let lan = document.querySelector("#selectLan").value;
 	let searchWord = document.querySelector("#keyword").value;
 	let kategori = document.querySelector("#selectKategori").value;
+	if (kategori == "99"){
+		kategori = "";
+	}
 	let url = `platsannonser/matchning?yrkesomradeid=${kategori}&nyckelord=${searchWord}&lanid=${lan}&antalrader=${numberOfDisplaysSelect}`;
 	searchByCriteria(url);
 })
@@ -144,26 +181,26 @@ function bindCardListeners() {
 	for (let card of cards) {
 
 		card.addEventListener("click", function(event){
-		/*	console.log(event.target)
-			console.log(this.target);
-			console.log(this.id);*/
-
-	const modal = document.getElementById('myModal');
-	const span = document.querySelector(".close");
-	modal.style.display = "block";
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
-
-	window.onclick = function(event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	}
-console.log(bindCardListeners);
+			/*	console.log(event.target)
+				console.log(this.target);
+				console.log(this.id);*/
 
 			let url = `platsannonser/${this.id}`
-			applicationInfo(url);
+				applicationInfo(url);
+
+			const modal = document.getElementById('myModal');
+			const span = document.querySelector(".close");
+			modal.style.display = "block";
+			span.onclick = function() {
+				modal.style.display = "none";
+			}
+
+			window.onclick = function(event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
+				
 		})
 	}
 }
